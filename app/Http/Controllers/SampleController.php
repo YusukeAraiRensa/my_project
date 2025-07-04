f0d7dbf7ef6e4c095dc018d82632b452f072e780
apple <apple@appurunoMacBook-Pro.local>
2025-07-04 11:32:09 +0900

    add #8

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -12,7 +12,7 @@ class SampleController extends Controller {
         // $insertUsersRecord = DB::connection('mysql')->insert("insert into users (id, email, name, password) values (Null, 'a@a.com', '山田太郎', 'password'), (Null, 'b@b.com', '山田花子', 'password'), (Null, 'c@c.com', '磯野カツオ', 'password')");
         // 挿入された内容を確認（最新3件を取得してダンプ）
         // $users = DB::table('users')->orderBy('id', 'desc')->take(3)->get();
-        // dd($users); // 確認用：追加されたユーザー情報が表示されます
+        // dd($users); // 確認用：追加されたユーザー情報が表示
 
         $deleteUserRecord = DB::connection('mysql')->delete("delete from users where name = '磯野カツオ'");
         dd($deleteUserRecord);
